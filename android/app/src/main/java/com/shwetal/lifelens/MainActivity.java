143bc52739f7241e8f47e517951aff42d5d3c080
Shwetal-more <shwetalmore275@gmail.com>
2025-09-06 02:00:00 +0530

    many changes made

--- android/app/src/main/java/com/shwetal/lifelens/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.shwetal.lifelens;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'lifelens',
-  webDir: 'dist'
-};
-
-export default config;
